c067e0b43c58548e8fc97dabd53b7a509c5f5c77
Touathi Vang <Touathi@outlook.com>
2023-05-05 22:12:02 -0500

    Forgot to commit... Linked and sourced script and jquery.

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(onReady);
+
+console.log('YAYY Its loaded');
+
+function onReady() {
+        console.log('Ready to run');
+
+
+    $('#submitBtn').on('click', handleSubmit)
+
+}
+
+
+function handleSubmit() {
+    
+    let firstNameInput = $('#firstNInput').val();
+    let lastNameInput = $('#lastNInput').val();
+    let eEIdInput = $('#eeIDInput').val();
+    let titleInput = $('#titleInput').val();
+    let anuSalInput = $('#annualSalInput').val();
+
+event.preventDefault();
+
+$('#eeBody').append(`
+
+    <tr>
+        <td>
+            ${firstNameInput}
+        </td>
+        <td>
+            ${lastNameInput}
+        </td>
+        <td>
+            ${}
+        </td>
+
+
+
+
+    </tr>
+`)
+
+}
