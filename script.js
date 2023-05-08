b4dba2d2b3df631c5dc5e3ef0e6526bb65970226
Touathi Vang <Touathi@outlook.com>
2023-05-07 20:07:50 -0500

    forgot to commit again, but I figured out why my it was not adding. I needed to created a variable and just have that one += the new input values. Also got the change color working, I forgot to capitalize the c in addclass. Added a code that would round up to two decimal places.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(onReady);
 
 
-let monthTotal;
+
 
 
 function onReady() {
@@ -11,23 +11,39 @@ function onReady() {
     $('#submitBtn').on('click', handleSubmit)
     $('#eeBody').on('click', '#deleteBtn', handleDelete)
 
+}
 
-    
-
-
-
+    // Round to nearest 2 decimals
+   
+let monthTotal = 0
+ let formattedMonthTotal = Math.round( monthTotal * 100) / 100
 // The submit button //
-function handleSubmit() {
+    function handleSubmit() {
     
 
-    // Getting the values of the inputs //
-    let firstNameInput = $('#firstNInput').val();
-    let lastNameInput = $('#lastNInput').val();
-    let eEIdInput = $('#eeIDInput').val();
-    let titleInput = $('#titleInput').val();
-    let anuSalInput = $('#annualSalInput').val();
+        // Getting the values of the inputs //
 
-event.preventDefault();
+        // Input for First Name value
+        let firstNameInput = $('#firstNInput').val();
+        // Input for Last name value
+        let lastNameInput = $('#lastNInput').val();
+        // Input for Employee ID Number
+        let eEIdInput = $('#eeIDInput').val();
+        // Input for Job Title
+        let titleInput = $('#titleInput').val();
+        // Input for Annual Salary 
+        let anuSalInput = $('#annualSalInput').val();
+
+        // As I input values and submit it It will show up as numbers with commas in it
+        // I understands that this seperate every 3 numbers and adds a "," in between it 
+        // but dont understand all the /(?=(?:\d{3})+$)/ stuff.
+        // what does all the / and ? \ means?
+        // let commaInAnuSalNumbers = anuSalInput.split(/(?=(?:\d{3})+$)/).join(",")
+        
+
+
+        // Refreshes the inpute values when the submit btn is clicked
+        event.preventDefault();
 
 // appending the input values and delete Btn into the table //
 $('#eeBody').append(`
@@ -46,7 +62,7 @@ $('#eeBody').append(`
             ${titleInput}
         </td>
         <td>
-            ${anuSalInput}
+            $ ${anuSalInput}
         </td>
         <td>
             <button id ='deleteBtn'>Delete</button>
@@ -54,12 +70,30 @@ $('#eeBody').append(`
     </tr>
 `)
 
+  
 
-    // Append the annual salary value into monthly total
-    monthTotal = anuSalInput
-    $('#monthlyTotal').text(monthTotal)
+    let anuSal = anuSalInput / 12
+    // Total Monthly and add newly added values
+    // formattedMonthTotal += anuSal
+//    monthTotal += anuSal
+   formattedMonthTotal += anuSal
+    // Change the Total Month everytime a new values is added
+    // $('#monthlyTotal').text(monthTotal) 
+    $('#monthlyTotal').text(formattedMonthTotal) 
+
+   
+
+
+
+
 
     // Add new appened Annual Sal value to Current monthly Total
+    //  $('#monthlyTotal').append(monthTotal);
+    // DO NOT NEED TO APPEND .TEXT IS ALREADY CHANGING IT THE TEXT
+    
+    
+
+    
    
 
 
@@ -69,18 +103,31 @@ $('#eeBody').append(`
     $('#eeIDInput').val('');
     $('#titleInput').val('');
     $('#annualSalInput').val('');
+
+    // if (monthTotal > 20000) {
+    //     $('#monthContainer').addClass('goRed');
+
+    // }
+
+
+
+  // TRIED If Statement, Change background color to red if 
+    monthTotal > 20000
+    if (formattedMonthTotal > 20000) {
+        $('#monthContainer').addClass('goRed');
+
+    }
+  
 }
 
-// The ❌/Delete button //
-function handleDelete() {
-    $(this).parent().parent().remove();
-}
-    
-    // TRIED If Statement, Change background color to red if 
-    // monthTotal > 20000
-    if (monthTotal > 20000) {
-        return $('#goRed').addclass("monthChangeColor");
+
+
+    // The Delete button //
+    function handleDelete() {
+        $(this).parent().parent().remove();
     }
-}
+    
+  
+
 
 console.log('YAYY Its loaded!!');
